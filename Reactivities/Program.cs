61a9adb2584eac25ac2d5bb230f3b32d573c364f
Omar AlKaraki <Omar.AlKaraki@omicron-jo.com>
2025-11-16 16:19:36 +0300

    Refactor to implement CQRS with MediatR and AutoMapper
    Refactored the application to adopt the CQRS pattern using MediatR, decoupling controller logic from the data access layer. Updated `ActivitiesController` to use MediatR commands and queries for activity operations. Added new endpoints for creating, editing, and deleting activities.
    
    Introduced AutoMapper for object mapping and added `MappingProfiles.cs` to configure mappings. Updated `Program.cs` to register MediatR and AutoMapper services, configure CORS, and simplify `AddDbContext` setup.
    
    Modified `DbInitializer.cs` to ensure database changes are saved. Updated `BaseApiController` to include a lazy-loaded `IMediator` property. Added MediatR command and query classes for activity operations.
    
    Updated `Application.csproj` to include references to `AutoMapper`, `MediatR`, and project dependencies. Updated database files to reflect schema or data changes.
    
    These changes improve maintainability, scalability, and developer experience.

--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -1,29 +1,27 @@
 using Persistence.DbContext;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
+using Application.Activities.Queries;
+using Application.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<AppDbContext>(options => { options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); });
+builder.Services.AddCors();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfiles>()); 
 
 var app = builder.Build();
 
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
-app.MapControllerRoute(
-    name: "root",
-    pattern: "",
-    defaults: new { controller = "WeatherForecast", action = "Get" });
-
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
+
 try
 {
     var context = services.GetRequiredService<AppDbContext>();
